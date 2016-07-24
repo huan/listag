63d8af3077f48218228af6257e64b5dd41c77538
Zhuohuan LI <zixia@zixia.net>
2016-07-24 10:50:17 +0800

    use module version of declaration file

--- index.d.ts
+++ index.d.ts
@@ -1,41 +1,41 @@
 // https://www.typescriptlang.org/docs/handbook/writing-declaration-files.html
 // https://github.com/Microsoft/TypeScript/issues/2076#issuecomment-75052599
 
-import { EventEmitter } from 'events'
+declare module "listag" {
+  import { EventEmitter } from 'events'
 
-interface ListagItem {
-  new (data: any): ListagItem
-  new (data: any, tagMap: Object): ListagItem
+  interface ListagItem {
+    new (data: any): ListagItem
+    new (data: any, tagMap: Object): ListagItem
 
-  tag(tagMap: Object): ListagItem
-  hasTag(tagMap: Object): boolean
+    tag(tagMap: Object): ListagItem
+    hasTag(tagMap: Object): boolean
+  }
+
+  export default interface Listag extends EventEmitter {
+    new (): Listag
+    new (item: any): Listag
+    new (item: any, tagMap: any): Listag
+    new (itemList: any[]): Listag
+    new (itemList: any[], tagMap: Object): Listag
+
+    length(): number
+
+    add(item: any): Listag | ListagItem
+    add(item: any, tagMap: Object): Listag | ListagItem
+    add(itemList: any[], tagMap: Object): Listag | ListagItem
+
+    del(item: any): number
+    del(itemList: any[]): number
+
+    get(tagMap: Object): Listag
+    tag(tagMap: Object): Listag
+
+    getTag(item: any): Object
+    getTag(itemList: any[]): Object
+
+    forEach(cb: Function): any
+    map(cb: Function): any
+    reduce(cb: Function, initialValue?: any): any
+  }
 }
-
-interface Listag extends EventEmitter {
-  new (): Listag
-  new (item: any): Listag
-  new (item: any, tagMap: any): Listag
-  new (itemList: any[]): Listag
-  new (itemList: any[], tagMap: Object): Listag
-
-  length(): number
-
-  add(item: any): Listag | ListagItem
-  add(item: any, tagMap: Object): Listag | ListagItem
-  add(itemList: any[], tagMap: Object): Listag | ListagItem
-
-  del(item: any): number
-  del(itemList: any[]): number
-
-  get(tagMap: Object): Listag
-  tag(tagMap: Object): Listag
-
-  getTag(item: any): Object
-  getTag(itemList: any[]): Object
-
-  forEach(cb: Function): any
-  map(cb: Function): any
-  reduce(cb: Function, initialValue?: any): any
-}
-
-export declare var Listag: Listag
